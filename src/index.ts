938e19a53ab7e2bae8fd87c68f6b52dca5660daf
DwanW <kunvang@yahoo.com>
2021-02-26 21:11:33 -0700

    added graphql crud operations

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import mikroOrmConfig from "./mikro-orm.config";
--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,12 +1,56 @@
-import { Post } from "src/entities/Post";
-import { MyContext } from "src/types";
-import { Resolver, Query, Ctx } from "type-graphql";
+import { Post } from "../entities/Post";
+import { MyContext } from "../types";
+import { Resolver, Query, Ctx, Arg, Int, Mutation } from "type-graphql";
 
 // resolvers for apollo graphql server
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
-  posts(@Ctx() { em }: MyContext) {
+  posts(@Ctx() { em }: MyContext): Promise<Post[]> {
     return em.find(Post, {});
+  }
+
+  @Query(() => Post, { nullable: true })
+  post(
+    @Arg("_id", () => Int) _id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    return em.findOne(Post, { _id });
+  }
+
+  @Mutation(() => Post)
+  async createPost(
+    @Arg("title", () => String) title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post> {
+    const post = em.create(Post, { title });
+    await em.persistAndFlush(post);
+    return post;
+  }
+
+  @Mutation(() => Post)
+  async updatePost(
+    @Arg("_id") _id: number,
+    @Arg("title", () => String, { nullable: true }) title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    const post = await em.findOne(Post, { _id });
+    if (!post) {
+      return null;
+    }
+    if (typeof title !== "undefined") {
+      post.title = title;
+      await em.persistAndFlush(post);
+    }
+    return post;
+  }
+
+  @Mutation(() => Boolean)
+  async deletePost(
+    @Arg("_id") _id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<boolean> {
+    await em.nativeDelete(Post, { _id });
+    return true;
   }
 }
